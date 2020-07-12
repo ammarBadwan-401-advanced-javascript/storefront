c835e6dd30ff80b5383ebaaaa9484315bcd7ef7f
Ammar <ammar.badwan95@gmail.com>
2020-07-12 14:29:03 +0300

    created the basic setup for the app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.scss';
+
+export default props =>{
+
+  return(
+    <h1>Hi</h1>
+  )
+}
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+const Main = props =>{
+
+  return(
+    <App/>
+  )
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<Main/>,root);
